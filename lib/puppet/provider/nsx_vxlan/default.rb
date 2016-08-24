3b4fb2b504f34580dfbc88749aa97a8ee417c46d
maniacmurphy <maniacmurphy@gmail.com>
2016-08-24 21:47:18 +0200

    Migrated type/provider nsx_vxlan

---
+++ lib/puppet/provider/nsx_vxlan/default.rb
@@ -0,0 +1,47 @@
+# Copyright (C) 2014 VMware, Inc.
+provider_path = Pathname.new(__FILE__).parent.parent
+require File.join(provider_path, 'nsx')
+
+Puppet::Type.type(:nsx_vxlan).provide(:default, :parent => Puppet::Provider::Nsx) do
+  @doc = 'Manage VXLAN Virtual Wires.'
+
+  def vxlan_wire
+    @vxlan_wire ||= vxlan_wire_config(resource[:name])
+  end
+
+  def exists?
+    vxlan_wire
+  end
+
+  def replace_properties
+    data = {}
+    Puppet::Type.type(:nsx_vxlan).parameters.collect.select {|x| x.to_s =~ /(name|description|tenant_id)/}.each do |prop|
+      if resource[prop]
+        camel_prop       = PuppetX::VMware::Util.camelize(prop, :lower)
+        data[camel_prop] = resource[prop]
+      end
+    end
+    data
+  end
+
+  def create
+    scope_id = vdn_scope['id']
+    post_url = "api/2.0/vdn/scopes/#{scope_id}/virtualwires"
+    post(post_url, { 'virtualWireCreateSpec' => replace_properties } )
+  end
+
+  def destroy
+    vwire_id = vxlan_wire['objectId']
+    delete("api/2.0/vdn/virtualwires/#{vwire_id}")
+  end
+
+  def vdn_scope
+    @vdn_scope ||= begin
+      scope_url = '/api/2.0/vdn/scopes'
+      results = ensure_array( nested_value(get(scope_url), %w{vdnScopes vdnScope}))
+      scope = results.find{|vdnScope| vdnScope['id']}
+      scope
+    end
+  end
+
+end
---
+++ lib/puppet/type/nsx_vxlan.rb
@@ -0,0 +1,44 @@
+# Copyright (C) 2014 VMware, Inc.
+require 'pathname'
+vmware_module = Puppet::Module.find('vmware_lib', Puppet[:environment].to_s)
+require File.join vmware_module.path, 'lib/puppet/property/vmware'
+
+Puppet::Type.newtype(:nsx_vxlan) do
+  @doc = 'Manage NSX VXLAN Virtual Wires.'
+
+  ensurable do
+    newvalue(:present) do
+      provider.create
+    end
+
+    newvalue(:absent) do
+      provider.destroy
+    end
+
+    defaultto(:present)
+  end
+
+  newparam(:name, :namevar => true) do
+    desc 'virtual wire name'
+    newvalues(/\w/)
+  end
+
+  newparam(:description) do
+    desc 'virtual wire description'
+    newvalues(/\w/)
+  end
+
+  newparam(:tenant_id) do
+    desc 'virtual wire tenant id'
+    newvalues(/\w/)
+  end
+
+  autorequire(:nsx_vxlan_scope) do
+    self[:name]
+  end
+
+  autorequire(:transport) do
+    self[:name]
+  end
+
+end
