4262e5c3112761f181a94209c58b2c018bd9ad26
maniacmurphy <maniacmurphy@gmail.com>
2016-08-24 21:19:29 +0200

    Migrated type/provider nsx_application

---
+++ lib/puppet/provider/nsx_application/default.rb
@@ -0,0 +1,67 @@
+# Copyright (C) 2014 VMware, Inc.
+provider_path = Pathname.new(__FILE__).parent.parent
+require File.join(provider_path, 'nsx')
+
+Puppet::Type.type(:nsx_application).provide(:default, :parent => Puppet::Provider::Nsx) do
+  @doc = 'Manages NSX application.'
+
+  def exists?
+    results = ensure_array( nested_value(get("/api/2.0/services/application/scope/#{nsx_scope_moref}"), ['list', 'application']) )
+    # If there's a single application the result is a hash, while multiple results in an array.
+    @application = results.find {|application| application['name'] == resource[:name]}
+  end
+
+  def create
+    data = {
+      :revision           => 0,
+      :name               => resource[:name],
+      :inheritanceAllowed => true,
+      :element            => { :value    => resource_value,
+                               :applicationProtocol => resource[:application_protocol],
+      }
+    }
+    post("api/2.0/services/application/#{nsx_scope_moref}", {:application => data} )
+  end
+
+  def destroy
+    delete("api/2.0/services/application/#{@application['objectId']}")
+  end
+
+  def value
+    @application['element']['value'].split(',')
+  end
+
+  def value=(ports)
+    @pending_changes = true
+  end
+
+  def application_protocol
+    @application['element']['applicationProtocol']
+  end
+
+  def application_protocol=(proto)
+    @pending_changes = true
+  end
+
+  def resource_value
+    resource[:value].join(',')
+  end
+
+  def flush
+    if @pending_changes
+      # requires us to increment revision number, thing to try in future is omitting revision key
+      @application['revision']                       = @application['revision'].to_i + 1
+      @application['element']['applicationProtocol'] = resource[:application_protocol]
+      @application['element']['value']               = resource_value
+
+      # get rid of nil value hash elements
+      data                      = {}
+      data[:application]        = @application.reject{|k,v| v.nil? }
+
+      Puppet.debug("Updating to #{resource_value}")
+      put("api/2.0/services/application/#{@application['objectId']}", data )
+
+    end
+  end
+
+end
---
+++ lib/puppet/type/nsx_application.rb
@@ -0,0 +1,60 @@
+# Copyright (C) 2014 VMware, Inc.
+require 'pathname'
+vmware_module = Puppet::Module.find('vmware_lib', Puppet[:environment].to_s)
+require File.join vmware_module.path, 'lib/puppet/property/vmware'
+
+Puppet::Type.newtype(:nsx_application) do
+  @doc = 'Manage NSX applications, these are used by fw rules'
+
+  ensurable
+
+  newparam(:name, :namevar => true) do
+    desc 'application name'
+  end
+
+  newproperty(:value, :array_matching => :all, :parent => Puppet::Property::VMware_Array, :sort => :true ) do
+    desc 'application value, this is a string that can consist of port number(s) and ranges of ports'
+    munge do |value|
+      # since nsx treats these as strings, we are doing the same, this is needed to account for ranges
+      value.to_s
+    end
+  end
+
+  newproperty(:application_protocol) do
+    desc 'application protocol, example TCP/UDP/ICMP/IGMP/FTP/etc ( way to many to list, refer to api guide for more available options'
+    munge do |value|
+      value.upcase
+    end
+  end
+
+  newparam(:scope_type) do
+    desc 'scope type, this can be either edge, datacenter, or global. if not specified, edge is the default'
+    newvalues(:edge, :datacenter, :global_root, :global)
+    defaultto(:edge)
+    munge do |value|
+      value = 'global_root' if value == 'global'
+      value
+    end
+  end
+
+  newparam(:scope_name) do
+    desc 'scope name which will be used with scope_type to get/set applications'
+  end
+
+  newparam(:inclusive) do
+    desc 'whether the resource value is inclusive'
+    newvalues(:true, :false)
+    defaultto(:true)
+  end
+
+  newparam(:preserve) do
+    desc 'whether existing resource values are preserved'
+    newvalues(:true, :false)
+    defaultto(:false)
+  end
+
+  autorequire(:nsx_edge) do
+    self[:name]
+  end
+
+end
