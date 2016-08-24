39f2b43192e4b48b515199f9426d1b6966ae038d
maniacmurphy <maniacmurphy@gmail.com>
2016-08-24 21:46:09 +0200

    Migrated type/provider nsx_ipset

---
+++ lib/puppet/provider/nsx_ipset/default.rb
@@ -0,0 +1,44 @@
+# Copyright (C) 2014 VMware, Inc.
+provider_path = Pathname.new(__FILE__).parent.parent
+require File.join(provider_path, 'nsx')
+
+Puppet::Type.type(:nsx_ipset).provide(:default, :parent => Puppet::Provider::Nsx) do
+  @doc = 'Manages NSX ipset.'
+
+  def exists?
+    results = ensure_array( nested_value(get("/api/2.0/services/ipset/scope/#{nsx_scope_moref(resource[:scope_type])}"), ['list', 'ipset']) )
+
+    # If there's a single ipset the result is a hash, while multiple results in an array.
+    @ipset = results.find {|ipset| ipset['name'] == resource[:name]}
+  end
+
+  def create
+    data = {
+      :revision => 0,
+      :name     => resource[:name],
+      :value    => resource[:value].sort.join(','),
+      :inheritanceAllowed => true,
+    }
+    post("api/2.0/services/ipset/#{nsx_scope_moref(resource[:scope_type])}", {:ipset => data} )
+  end
+
+  def destroy
+    delete("api/2.0/services/ipset/#{@ipset['objectId']}")
+  end
+
+  def value
+    @ipset['value'].split(',').sort
+  end
+
+  def value=(ips)
+    data = @ipset.clone
+    # requires us to increment revision number, thing to try in future is omitting revision key
+    data['revision'] = Integer(data['revision']) + 1
+    data['value']    = resource[:value].sort.join(',')
+    # get rid of nil value hash elements
+    data = data.reject{|k,v| v.nil?}
+
+    put("api/2.0/services/ipset/#{@ipset['objectId']}", {:ipset => data} )
+  end
+
+end
---
+++ lib/puppet/type/nsx_ipset.rb
@@ -0,0 +1,49 @@
+# Copyright (C) 2014 VMware, Inc.
+require 'pathname'
+vmware_module = Puppet::Module.find('vmware_lib', Puppet[:environment].to_s)
+require File.join vmware_module.path, 'lib/puppet/property/vmware'
+
+Puppet::Type.newtype(:nsx_ipset) do
+  @doc = 'Manage NSX ipsets, these are used by fw rules'
+
+  ensurable
+
+  newparam(:name, :namevar => true) do
+    desc 'ipset name'
+  end
+
+  newproperty(:value, :array_matching => :all, :parent => Puppet::Property::VMware_Array ) do
+    desc 'ipset value, this is a string that can consist of ip addresses, networks, and range of addresses seperated with commas'
+  end
+
+  newparam(:scope_type) do
+    desc 'scope type, this can be either edge, datacenter, or global. if not specified, edge is the default'
+    newvalues(:edge, :datacenter, :global_root, :global)
+    defaultto(:edge)
+    munge do |value|
+      value = 'global_root' if value == 'global'
+      value
+    end
+  end
+
+  newparam(:scope_name) do
+    desc 'scope name which will be used with scope_type to get/set ipsets'
+  end
+
+  newparam(:inclusive) do
+    desc 'whether the resource value is inclusive'
+    newvalues(:true, :false)
+    defaultto(:true)
+  end
+
+  newparam(:preserve) do
+    desc 'whether existing resource values are preserved'
+    newvalues(:true, :false)
+    defaultto(:false)
+  end
+
+  autorequire(:nsx_edge) do
+    self[:name]
+  end
+
+end
